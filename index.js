53327df98ff8cc6e330359dc73e094d61a54ab79
sai_devops_89 <itssaivivek@gmail.com>
2026-04-28 00:10:45 +0530

    Update server response message in index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** Hey my app was deployed :) **'); //write a response to the client
+  res.write('** Hey my app code is coding :) **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
